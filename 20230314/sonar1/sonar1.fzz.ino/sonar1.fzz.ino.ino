944fcc2ee92473943fd63cea4a12415cc428666b
songuijun <125720533+songuijun@users.noreply.github.com>
2023-03-14 15:36:48 +0900

    Add files via upload

---
+++ 20230314/sonar1/sonar1.fzz.ino/sonar1.fzz.ino.ino
@@ -0,0 +1,43 @@
+#define TRIG1 3 
+#define ECHO1 4
+
+#define TRIG1 3 
+#define ECHO1 4
+
+#define TRIG1 3 
+#define ECHO1 4
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(TRIG1, OUTPUT);
+  pinMode(ECHO1, INPUT);
+  Serial.begin(115200); //통신속도 115200으로 정의함
+
+}
+long sonar1(void)//초음파 센서 1번 측정 함수
+{
+  long duration, distance;
+  digitalWrite(TRIG1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG1, LOW);
+  duration = pulseIn(ECHO1, HIGH);
+  distance = ((float)(340*duration)/1000)/2;
+  return distance;
+  
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  long duration, distance;
+  digitalWrite(TRIG1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG1, LOW);
+  duration = pulseIn(ECHO1, HIGH);
+  distance = ((float)(340*duration)/1000)/2;
+  Serial.print("Duration: ");
+  Serial.println(duration);
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(500);
+
+}
