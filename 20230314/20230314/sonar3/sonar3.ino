c9f37ceb2864d5ae63a229f58318c2c4c6b40f01
songuijun <125720533+songuijun@users.noreply.github.com>
2023-04-10 08:59:18 +0900

    Update sonar3.ino

--- 20230314/20230314/sonar3/sonar3.ino
+++ 20230314/20230314/sonar3/sonar3.ino
@@ -55,13 +55,7 @@ long sonar3(void)
 }
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-  long duration, distance;
-  digitalWrite(TRIG1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG1, LOW);
-  duration = pulseIn(ECHO1, HIGH);
-  distance = (  (float)(340*duration)/1000)/2;
+
   //Serial.print("Duration: ");
   //Serial.println(duration);
   Serial.print("Distance1: ");
