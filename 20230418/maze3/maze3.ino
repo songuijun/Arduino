43fb5bf9794558f34f980557bfde9fbd4ce03c8b
songuijun <125720533+songuijun@users.noreply.github.com>
2023-05-03 21:36:57 +0900

    Update maze3.ino

--- 20230418/maze3/maze3.ino
+++ 20230418/maze3/maze3.ino
@@ -12,17 +12,17 @@
 #define Left  1
 #define Right 2
 
-#define TRIG1 2 //초음파 센서 1번 Trig 핀 번호
-#define ECHO1 3 //초음파 센서 1번 Echo 핀 번호
+#define TRIG1 2
+#define ECHO1 3
 
-#define TRIG2 4 //초음파 센서 2번 Trig 핀 번호
-#define ECHO2 5 //초음파 센서 2번 Echo 핀 번호
+#define TRIG2 4
+#define ECHO2 5
 
-#define TRIG3 6 //초음파 센서 3번 Trig 핀 번호
-#define ECHO3 7 //초음파 센서 3번 Echo 핀 번호
+#define TRIG3 6
+#define ECHO3 7
 
-NewPing sonar[SONAR_NUM] = {   // Sensor object array.
-  NewPing(TRIG1, ECHO1, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+NewPing sonar[SONAR_NUM] = {   
+  NewPing(TRIG1, ECHO1, MAX_DISTANCE), 
   NewPing(TRIG2, ECHO2, MAX_DISTANCE),
   NewPing(TRIG3, ECHO3, MAX_DISTANCE)
 };
@@ -30,18 +30,16 @@ NewPing sonar[SONAR_NUM] = {   // Sensor object array.
   float left_sonar = 0.0;
   float right_sonar = 0.0;
 
-
 #define ENA 8
 #define IN1 9
 #define IN2 10
 #define IN3 11
 #define IN4 12
 #define ENB 13
-
 int maze_status = 0;
 
 
-void setup()
+void setup() 
 {
    
     pinMode(ENA, OUTPUT);
@@ -55,7 +53,7 @@ void setup()
     Serial.begin(115200); //통신속도를 115200으로 정의함
 }
 
-void motor_A_control(int direction_a, int motor_speed_a) //모터 A의 방향과 속도제어
+void motor_A_control(int direction_a, int motor_speed_a) //모터 A의 방향(direction)과 속도(speed)제어
 {
   if(direction_a == HIGH)
   {
@@ -71,13 +69,13 @@ void motor_A_control(int direction_a, int motor_speed_a) //모터 A의 방향과
   }
 }
 
-void motor_B_control(int direction_b, int motor_speed_b)
+void motor_B_control(int direction_b, int motor_speed_b) //모터 B의 방향(direction)과 속도(speed)제어
 {
   if(direction_b == HIGH)
   {
-      digitalWrite(IN3,LOW);          // 모터의 방향 제어
-      digitalWrite(IN4,HIGH);           // 모터의 방향 제어
-      analogWrite(ENB,motor_speed_b);  // 모터의 속도 제어
+      digitalWrite(IN3,LOW);          
+      digitalWrite(IN4,HIGH);        
+      analogWrite(ENB,motor_speed_b); 
   }
   else
   {
@@ -111,13 +109,13 @@ void check_maze_status(void) {
 void wall_collision_avoid(int base_speed)
 {
   float error = 0.0;
-  float Kp = 0.3;       //나중에 조정해야 할 것
-  int pwm_control = 0; // pwm을 0으로 초기화
-  int right_pwm = 0;  // 모터속도 0으로 초기화
-  int left_pwm = 0;   // 모터속도 0으로 초기화
- 
+  float Kp = 0.3;      
+  int pwm_control = 0; 
+  int right_pwm = 0;  
+  int left_pwm = 0; 
+  
   error = (right_sonar - left_sonar); // 벽면 간의 거리
-  error = Kp * error; // 스티어 양 조절
+  error = Kp * error; // 스티어 양 조절 
 
   if (error >=  40) error =  40; // 과도한 스티어링 방지
   if (error <= -40) error = -40;
@@ -139,7 +137,7 @@ void wall_collision_avoid(int base_speed)
 void loop() {
   // put your main code here, to run repeatedly:
 
-  front_sonar = sonar[Front].ping_cm() * 10; //전방 센서 측정
+  front_sonar = sonar[Front].ping_cm() * 10; //전방 센서 측정 
   left_sonar = sonar[Left].ping_cm() * 10;   //좌측 센서 측정
   right_sonar = sonar[Right].ping_cm() * 10; //우측 센서 측정
    
@@ -152,10 +150,71 @@ void loop() {
   Serial.print("R: "); Serial.println(right_sonar);
 
   check_maze_status();
-  if( maze_status == 1)
-  {
+  if( maze_status == 4)
+  { 
+     motor_A_control(HIGH,0); //오른쪽 전진
+    motor_B_control(LOW,0); //왼쪽은 후진
+    delay(500);
+   
+    Serial.println("Rotate CCW");
+    motor_A_control(HIGH,100); //오른쪽 전진
+    motor_B_control(LOW,100); //왼쪽은 후진
+    delay(1100);               //일정한 시간 동안 회전
+ 
+    Serial.println("Rotate stop");
+    motor_A_control(HIGH,0); //오른쪽 전진
+    motor_B_control(LOW,0); //왼쪽은 후진
+    delay(500);
+  }
+  else if( maze_status == 1)
+  { // 좌우 벽만 있을 때 직전
+    Serial.println("Go straight");
+    wall_collision_avoid(MOTOR_SPEED);
+    
+  }
+  else if( maze_status == 2)
+  { 
+     motor_A_control(HIGH,0); //오른쪽 전진
+    motor_B_control(LOW,0); //왼쪽은 후진
+    delay(500);
+   
+    Serial.println("Rotate CCW");
+    motor_A_control(LOW,100); //오른쪽 전진
+    motor_B_control(HIGH,100); //왼쪽은 후진
+    delay(550);               //일정한 시간 동안 회전
+ 
+   Serial.println("Rotate stop");
+    motor_A_control(HIGH,0); //오른쪽 전진
+    motor_B_control(LOW,0); //왼쪽은 후진
+    delay(1000);
+   
     Serial.println("Go Straight");
+    motor_A_control(HIGH,50);
+    motor_B_control(HIGH,50);
+    delay(500);
+    
+  }
+  else if( maze_status == 3)
+  {
+    motor_A_control(HIGH,0); //오른쪽 전진
+    motor_B_control(LOW,0); //왼쪽은 후진
+    delay(500);
+   
+    Serial.println("Rotate CCW");
+    motor_A_control(HIGH,100); //오른쪽 전진
+    motor_B_control(LOW,100); //왼쪽은 후진
+    delay(550);               //일정한 시간 동안 회전
+ 
+    Serial.println("Rotate stop");
+    motor_A_control(HIGH,0); //오른쪽 전진
+    motor_B_control(LOW,0); //왼쪽은 후진
+    delay(500);
+  }
+  else 
+  {
+    // 직진
+    Serial.println("Go straight");
     wall_collision_avoid(MOTOR_SPEED);
   }
-   
+
 }
