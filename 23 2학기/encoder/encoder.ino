7e79999e80d71ae662fc9358de8cd2b120ec4048
songuijun <125720533+songuijun@users.noreply.github.com>
2023-09-04 09:53:34 +0900

    Add files via upload

---
+++ 23 2학기/encoder/encoder.ino
@@ -0,0 +1,69 @@
+#define ENA 4
+#define IN1 5
+#define IN2 6
+#define IN3 7
+#define IN4 8
+#define ENB 9
+//////엔코터 모터////////
+#define EN11 19
+#define EN12 18 //오른쪽
+
+#define EN21 2
+#define EN22 3 //왼쪽
+
+void setup()
+{
+  pinMode (ENA, OUTPUT);
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode (ENB, OUTPUT);
+
+  Serial.begin(115200);
+}
+
+void motor_R_control(int motor_speed_R) // 모터 A의 속도(speed)제어
+{
+  if (motor_speed_R >= 0)
+  {
+    digitalWrite(IN3, LOW);         //모터의 방향 제어
+    digitalWrite(IN4, HIGH);
+    if (motor_speed_R >= 255) motor_speed_R = 255;
+    analogWrite(ENB, motor_speed_R); //모터의 속도 제어
+  }
+  else
+  {
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    if (motor_speed_R <= -255) motor_speed_R = -255;
+    analogWrite(ENB, -motor_speed_R);
+  }
+}
+
+
+void motor_L_control(int motor_speed_L) // 모터 A의 속도(speed)제어
+{
+  if (motor_speed_L >= 0)
+  {
+    digitalWrite(IN1, LOW);         //모터의 방향 제어
+    digitalWrite(IN2, HIGH);
+    if (motor_speed_L >= 255) motor_speed_L = 255;
+    analogWrite(ENA, motor_speed_L); //모터의 속도 제어
+  }
+  else
+  {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    if (motor_speed_L <= -255) motor_speed_L = -255;
+    analogWrite(ENA, -motor_speed_L);
+  }
+}
+
+
+void loop()
+{
+  motor_R_control(120);
+  motor_L_control(120);
+}
